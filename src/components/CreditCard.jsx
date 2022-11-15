ca9f3be8c73791cbe0ff18c901759b07280cd1b9
Milky Raven <heartonllamas@gmail.com>
2022-11-15 15:58:59 +0100

    lab-finished

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
+import VisaLogo from '../assets/images/visa.png'
 
 export default function CreditCard(props) {
-    const {type, number, expirationMonth, expirationYear, bank, owner, bgColor, color} = props;
+    const { type, number, expirationMonth, expirationYear, bank, owner, bgColor, color } = props;
+    let last4Digits = number.slice(-4);
+    let maskedNumber = last4Digits.padStart(number.length, "·");
     const divStyle = {
         backgroundColor: `${bgColor}`,
         color: `${color}`,
         width: 400,
-        height: 160,
+        height: 200,
         margin: 10
     }
     return (
-    <div style={divStyle}>
-        <p>{type}</p>
-        <p>{number}</p>
-        <p>{expirationMonth}</p>
-        <p>{expirationYear}</p>
-        <p>{bank}</p>
-        <p>{owner}</p>
-    </div>
-  )
+        <div className='creditCard' style={divStyle}>
+            <img id='card-logo' src={type === 'Visa'? VisaLogo : VisaLogo} alt="" />
+            <h2>{maskedNumber}</h2>
+            <div className='details'>
+                <p>{expirationMonth}</p>
+                <p>{expirationYear}</p>
+                <p>{bank}</p>
+            </div>
+            <p>{owner}</p>
+        </div>
+    )
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+// import {faStar} from '@fortawesome/free-regular-svg-icons';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
+import {faStar as farStar} from '@fortawesome/free-regular-svg-icons';
 
 export default function Rating(props) {
     let rating = Math.round(props.children);
     let starRating;
     if (rating === 0){
-      starRating = <div>*Imagine this is an empty star*</div>
+      starRating = <div><FontAwesomeIcon icon={farStar} /><FontAwesomeIcon icon={farStar} /><FontAwesomeIcon icon={farStar} /><FontAwesomeIcon icon={farStar} /><FontAwesomeIcon icon={farStar} /></div>
     } else if (rating === 1) {
-      starRating = <div><FontAwesomeIcon icon={faStar} /></div>
+      starRating = <div><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={farStar} /><FontAwesomeIcon icon={farStar} /><FontAwesomeIcon icon={farStar} /><FontAwesomeIcon icon={farStar} /></div>
     } else if (rating === 2){
-      starRating = <div><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></div>
+      starRating = <div><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={farStar} /><FontAwesomeIcon icon={farStar} /><FontAwesomeIcon icon={farStar} /></div>
     } else if (rating === 3){
-      starRating = <div><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></div>
+      starRating = <div><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={farStar} /><FontAwesomeIcon icon={farStar} /></div>
     } else if (rating === 4) {
-      starRating = <div><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></div>
+      starRating = <div><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={farStar} /></div>
     } else if (rating === 5){
       starRating = <div><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></div>
     }
